1846d5175ad15307b461ecdb662c1584ff8598a2
Jonathan Delgado <hi@jon.soy>
2021-06-02 16:21:24 -0400

    feat: configure sentry integration

--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,14 @@ fn args_to_vec() -> Vec<String> {
 }
 
 fn main() {
+  let _guard = sentry::init((
+    "https://aff1dae4a3254225909afb3017eb387e@o345030.ingest.sentry.io/5797907",
+    sentry::ClientOptions {
+      release: sentry::release_name!(),
+      ..Default::default()
+    },
+  ));
+
   let args = args_to_vec();
   let action = args.get(1);
   let next_args = args.get(2..);
